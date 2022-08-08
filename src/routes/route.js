cb1a04c1bebed7b0bcc52b6325e87b9c4c61bf55
raj3028 <rajeshkumarsahoo006@gmail.com>
2022-08-08 16:13:28 +0530

    Problem-4

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const express1 = require("lodash");
 const myPage = require("../logger/logger");
 const myPageHelper = require("../util/helper");
 const myPageFormatter = require("../validator/formatter");
@@ -11,6 +12,48 @@ router.get("/test-me", function (req, res) {
   myPageHelper.date();
   console.log("_________________________" + "Problem 3");
   myPageFormatter.trim();
+  console.log("_________________________" + "Problem 4");
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  console.log(express1.chunk(months, [4]));
+  console.log("_________________________");
+
+  const oddNum = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+  console.log(express1.tail(oddNum, []));
+  console.log("_________________________");
+
+  const arr = [2];
+  const arr1 = [2];
+  const arr2 = [2];
+  const arr3 = [2];
+  const arr4 = [2];
+  const Result = express1.union(arr, arr1, arr2, arr3, arr4);
+  console.log(Result);
+  console.log("_________________________");
+
+  const pairs = [
+    ["horror", "The Shining"],
+    ["drama", "Titanic"],
+    ["thriller", "Shutter Island"],
+    ["fantasy", "Pans Labyrinth"],
+  ];
+  const obj = express1.fromPairs(pairs);
+  console.log(obj);
+  console.log("_________________________");
+
   res.send(
     "Welcome to my application. I am " +
       myPage.name +
