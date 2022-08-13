f2a90a3d3912d8489876a3a76593c41f87848783
raj3028 <rajeshkumarsahoo006@gmail.com>
2022-08-13 19:06:23 +0530

    NodeJs - Schema Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-
+const UserModel = require("../models/userModel.js");
+const UserController = require("../controllers/userController");
+// =================Book-Assignment========================
+const bookingData = require("../bookApi/api");
+router.post("/usersBookData", bookingData.booksName);
+router.get("/getAllBooksData", bookingData.allBooksName);
+// ========================================================
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+  res.send("My first ever api!");
+});
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createUser", UserController.createUser);
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getUsersData", UserController.getUsersData);
 
 module.exports = router;
