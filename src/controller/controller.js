6d43b95a1ffb3caaf58aafa0a85bb127101d2230
raj3028 <rajeshkumarsahoo006@gmail.com>
2022-08-18 11:26:42 +0530

    MongoDB session 3 - Assignment Submission

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -17,7 +17,7 @@ const getData = async function (req, res) {
   let allData = await authors
     .findOne({ author_name: "Chetan Bhagat" })
     .select({ author_id: 1 });
-  let getBookData = await books.find({ author_id: allData.author_id });
+  let getBookData = await books.find({ author_id: { $eq: allData.author_id } });
   res.send({ allBookInfo: getBookData });
 };
 // ==========================findAuthorData4=============================
@@ -39,8 +39,23 @@ const getResult = async function (req, res) {
 // ==========================findBook5=============================
 
 const findBook = async function (req, res) {
-  let allData = await (await authors.find({ price: { $gte: 50, $lte: 100 } }))
-  res.send({ allBookInfo: allData });
+  let allData = await books.find({ price: { $gte: 50, $lte: 100 } });
+
+  let fetch = allData.map((x) => x.author_id);
+  let newData = await authors
+    .find({ author_id: fetch })
+    .select({ author_name: 1, _id: 0 });
+  // let authorrs = [];
+  // for (let i = 0; i < allData.length; i++) {
+  //   let arr = await authors.find({ author_id: allData[i].author_id });
+  //   authorrs.push(arr);
+  // }
+  // let newData = {};
+  // for (let i = 0; i < allData.length; i++) {
+  //   newData[allData[i].name] = authorrs[i][0].author_name;
+  // }
+
+  res.send({ allBookInfo: newData });
 };
 
 // =================================Export-Modules====================================
